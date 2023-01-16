f13f2d5aa028e865bb6c010cedb2b2e3b1f130d5
sutanarief <sutan1412@gmail.com>
2023-01-16 22:52:48 +0700

    feature/LIN-3 add api post user

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const cors = require('cors')
 const express = require('express')
+const { connect } = require('./config')
+const dotenv = require('dotenv')
+const router = require('./routes')
 const app = express()
 const PORT = 3000
+
+dotenv.config()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(router)
 
-app.listen(PORT, () => {
-  console.log('Listening on PORT: ', PORT)
+connect().then(async (db) => {
+  const getList = await db.listCollections().toArray()
+  const listCollections = getList.map((x) => x.name)
+  const dbInfo = {
+    database: db.namespace,
+    collections: listCollections
+  }
+  console.log('Connected to Mongo with :', dbInfo)
+  
+  app.listen(PORT, () => {
+    console.log('Listening on PORT: ', PORT)
+  })
 })
